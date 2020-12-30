441bc3dc20eee8fcb7845f6f93875483b84aa3e5
Nurbiye <nurbiye720@gmail.com>
2020-12-29 23:34:54 -0600

    Just2 days class, have to refresh later...it is useful

--- src/test/java/serenity/VoilaTest.java
+++ src/test/java/serenity/VoilaTest.java
@@ -2,20 +2,30 @@ package serenity;
 
 
 import net.serenitybdd.junit5.SerenityTest;
+import net.thucydides.core.annotations.Steps;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import serenity.steps.B20Actions;
 
 @SerenityTest
+@Tag("voila")
 public class VoilaTest {
 
+    @Steps
+    B20Actions b20user;
 
     @Test
     public void testVoila(){
-
         //B20User preparedSomething
-
         //B20User takeAnAction
-
         //B20User expectSomeResults
+
+        //GIVEN
+        b20user.preparedSomething();
+        //WHEN
+        b20user.takeAnAction();
+        //THEN
+        b20user.expectSomeResult();
 
 
 
